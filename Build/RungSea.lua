b6d197951f2e1788b06c436775bacf275c244ac9
Secret4869 <zerotwov628@gmail.com>
2024-09-27 22:15:35 +0700

    Update RungSea.lua

--- Build/RungSea.lua
+++ Build/RungSea.lua
@@ -1,5 +1,5 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Secret4869/Secret/refs/heads/main/Library/Test.lua"))()
-local Window = redzlib:MakeWindow({"Exclusive Hub : Rung Piece", "By Tomie", "Exclusive_Rung_Piece.json"})
+local Window = redzlib:MakeWindow({"Exclusive Hub : Rung Piece", "By Tomie", "Exclusive_Rung_Sea.json"})
   Window:AddMinimizeButton({
     Button = { Image = redzlib:GetIcon("rbxassetid://18483333121"), BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(0, 6) }
@@ -75,7 +75,7 @@ Discord:AddDiscordInvite({
     Invite = "https://discord.gg/KMK6gXevWV"
 })
 Setting:AddSection("Settings")
-local Select_W = AddDropdown(Setting,languageData["Select Weapon"],"", Weaponlist, {Weaponlist[1]},"Select_ToolFarm")
+local Select_W = AddDropdownS(Setting, languageData["Select Weapon"], "", Weaponlist, {Weaponlist[1]}, true, "Select_ToolFarm")
 Setting:AddButton({languageData["Refresh Weapon"], function()
 Weaponlist = {}
     for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
@@ -87,29 +87,20 @@ Weaponlist = {}
         end
     Select_W:Set(Weaponlist)
 end})
+AddToggle(Setting, "Automatic Attack + Automatic Equip", "", Settings.Atk_E, "Atk_ATMEQ")
 Setting:AddDropdown({languageData["Select Method"], Mode, {Mode[3]}, function(call)
     Settings.Method = call
 end, "Select_Method"})
 AddSlider(Setting, languageData["Farm Distance"],"", 0, 100, Settings.FarmDistance, "Farm_Distance")
 Setting:AddSection(languageData["Automatic Skill"])
-Setting:AddToggle({"Z", Settings.Atomatic_SkillZ, function(call)
-    Settings.Atomatic_SkillZ = call
-end, "Atomatic_SkillZ"})
-Setting:AddToggle({"X", Settings.Atomatic_SkillX, function(call)
-    Settings.Atomatic_SkillX = call
-end, "Atomatic_SkillX"})
-Setting:AddToggle({"C", Settings.Atomatic_SkillC, function(call)
-    Settings.Atomatic_SkillC = call
-end, "Atomatic_SkillC"})
-Setting:AddToggle({"V", Settings.Atomatic_SkillV, function(call)
-    Settings.Atomatic_SkillV = call
-end, "Atomatic_SkillV"})
+AddToggle(Setting, "Z", "", Settings.Automatic_SkillZ, "Automatic_SkillZ")
+AddToggle(Setting, "X", "", Settings.Automatic_SkillX, "Automatic_SkillX")
+AddToggle(Setting, "C", "", Settings.Automatic_SkillC, "Automatic_SkillC")
+AddToggle(Setting, "V", "", Settings.Automatic_SkillV, "Automatic_SkillV")
 --------// [ General ] \\--------
 General:AddSection("Raid")
-General:AddButton({languageData["Tp Raid"], function()
-    Tp(position_raid)
-end})
-AddToggle(General, languageData["Automatic Clear RaidTrash"], " Cannot Instant Kill", Settings.Atomatic_Clear_RaidTrash, "Atomatic_Clear_RaidTrash")
+AddToggle(General, languageData["Automatic Clear Eazy BossFight"], "", Settings.Atomatic_Clear_Trash_Eazy, "Atomatic_Clear_Trash_Eazy")
+AddToggle(General, languageData["Automatic Clear Hard BossFight"], "", Settings.Atomatic_Clear_Trash_Hard, "Atomatic_Clear_Trash_Hard")
 General:AddSection(languageData["Automatic Spawn Boss + Automatic Farm Boss"])
 for _, bossData in pairs(LoadBoss) do
     AddToggle(General, "Automatic Spawn Boss " .. bossData[3], "Need <font color='rgb(252, 1, 127)'>".. bossData[1] .."</font>   If you don't have it, you'll buy it for me automatically", Settings[bossData[3]], "Automatic_Spawn_Boss_" .. bossData[3])
@@ -134,18 +125,20 @@ MobsList = {}
 end})
 AddToggle(General, languageData["Automatic Farm Mobs Select"], "", Settings.Atomatic_Level, "Atomatic_Level")
 --------// [ Item ] \\--------
-AddToggle(Item, languageData["Automatic Get Guntip Beta Test"], "", Settings.Atomatic_GetGuntip, "Atomatic_GetGuntip")
-AddToggle(Item, languageData["Automatic Get Sang Tin ja Test"], "", Settings.Atomatic_Get_STJ, "Atomatic_Get_STJ")
+AddToggle(Item, languageData["Automatic Craft Guntip Beta Test"], "", Settings.Atomatic_GetGuntip, "Atomatic_GetGuntip")
+AddToggle(Item, languageData["Automatic Craft Sang Tin JA"], "<font color='rgb(252, 1, 127)'> It will find everything necessary for making Sang Tin JA, except for the gem. You will have to find the gem yourself. </font>", Settings.Atomatic_Get_STJ, "Atomatic_Get_STJ")
+AddToggle(Item, languageData["Automatic Craft Cyn"], "", Settings.Automatic_Craft_Cya, "Automatic_Craft_Cya")
 --------// [ Teleport ] \\--------
 for _, prompt in pairs(workspace.Seller:GetDescendants()) do
     if prompt:IsA("ProximityPrompt") then
         Teleport:AddButton({prompt.ActionText, function()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = prompt.Parent.CFrame
+            local targetPivot = prompt.Parent:GetPivot()
+            game.Players.LocalPlayer.Character:PivotTo(targetPivot)
         end})
     end
 end
 --------// [ Miscellaneous ] \\--------
-AddToggle(Miscellaneous, languageData["Instant Kill"], "<font color='rgb(252, 1, 127)'>".. languageData["instant Kill Work On Boss Spawn and Normal Boss and if ping Too much server won't work "] .."</font>", Settings.Instant_Kill, "Instant Kill")
+AddToggle(Miscellaneous, languageData["Instant Kill"], "<font color='rgb(252, 1, 127)'>".. languageData["instant Kill Work On Boss Spawn and Normal Boss and if ping Too much server won't work and Cannot Instant Kill BossFight"] .."</font>", Settings.Instant_Kill, "Instant Kill")
 AddToggle(Miscellaneous, languageData["Automatic Grab Item Not recommended"], "", Settings.Automatic_GrabItem, "Automatic_GrabItem")
 Miscellaneous:AddButton({languageData["Redeem All Codes"], function()
     for _,v in pairs(localplayer.Codes:GetChildren()) do
